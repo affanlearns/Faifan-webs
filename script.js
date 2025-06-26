c7b280ed6df2d0d9ce0950814bc6b59b406dc74d
affanlearns <affanhassan2369@gmail.com>
2025-06-26 18:19:13 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function () {
-  const dropdown = document.querySelector('.dropdown');
-  dropdown.addEventListener('mouseenter', () => {
-    dropdown.querySelector('.dropdown-content').style.display = 'block';
-  });
-  dropdown.addEventListener('mouseleave', () => {
-    dropdown.querySelector('.dropdown-content').style.display = 'none';
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.nav-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
   });
 });
